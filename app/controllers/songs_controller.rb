3c04d9be510108862a980b19e989547c3ef77fd3
Howard DeVennish <howarddevennish@gmail.com>
2017-01-31 21:41:32 +0000

    work on create song form and textarea

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,15 @@
 class SongsController < ApplicationController
 
+  get '/songs' do
+    'get songs'
+  end
+
+  get '/songs/create_song' do
+    erb :'songs/create_song'
+  end
+
+  post '/songs' do
+    binding.pry
+    'post songs'
+  end
 end
